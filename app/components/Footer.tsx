8242c6a51f81695711ccc969de1960f1a01940a7
tonwareemi zakana <tonwareemizakana@gmail.com>
2024-10-29 13:04:14 +0100

    finish with landing page

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className=" bg-[url('/footerBackground.png')] w-screen h-[500px] pt-0 flex justify-evenly items-center flex-col">
+    <div className=" bg-[url('/footerBackground.png')] w-screen h-[500px] pt-0 flex justify-evenly items-center flex-col relative">
       <div className="flex items-center justify-evenly flex-col">
         <img src="/logo.png" alt=" " className="h-auto w-[6rem]" />
         <p className="text-white">Gifted natus Foundation</p>
@@ -9,13 +9,14 @@ export default function Footer() {
         <h1 className="text-3xl lg:text-3xl text-left text-white font-gobold font-bold w-[60%]">
           VISIT US
         </h1>
-        <div>
+        <div className="flex items-center justify-evenly">
           <img src="/Location.png" alt=" location image" className="w-[20px] h-[20px]" />{" "}
-          <p>No 4 , off Aje street, Ononuwa, Lagos state, Nigeria</p>
+          <p className="text-white w-[80%]">No 2, old Assembly Quarters,Ekeki Housing Estate, Yenagoa, Bayelsa state Nigeria</p>
         </div>
       </div>
-      <div className="">
+      <div className="flex items-start justify-evenly absolute bottom-5 left-5">
         <img src="/phone.png" alt="" className="w-5 h-5"/>
+        <p className="text-white">+2348162473903</p>
       </div>
     </div>
   );
