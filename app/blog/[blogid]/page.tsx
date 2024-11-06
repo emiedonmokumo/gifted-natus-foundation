b598f58d9a363f0ac8f080370120dd9d6c86d5c9
tonwareemi zakana <tonwareemizakana@gmail.com>
2024-11-06 12:23:51 +0100

    set type of params in blog

--- app/blog/[blogid]/page.tsx
+++ app/blog/[blogid]/page.tsx
@@ -1,13 +1,20 @@
-import Nav from "../../components/Nav"
-import Footer from "../../components/Footer" 
+import Nav from "../../components/Nav";
+import Footer from "../../components/Footer";
 
-export default function BlogPost ({params}){
-    return( <div>
-        <header className="bg-slate-900">
-            <Nav/>
-        </header>
-        <Footer/>
+export default function BlogPost({
+  params,
+}: {
+  params: {
+    blogid: string;
+    userid: string;
+  };
+}) {
+  return (
+    <div>
+      <header className="bg-slate-900">
+        <Nav />
+      </header>
+      <Footer />
     </div>
-
-    )
+  );
 }
