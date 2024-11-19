3b20ecb14d76eeec02f1f81ca9c7b1e59e2adbc3
Emie-Trialer <emiedonmukumo@gmail.com>
2024-11-19 11:41:19 +0100

    update

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -23,6 +23,6 @@ export async function POST(req: NextRequest) {
         );
     } catch (error: any) {
         console.error("Error:", error);
-        return NextResponse.json({ message: error.message }, { status: 500 });
+        return NextResponse.json({ message: error }, { status: 500 });
     }
 }
