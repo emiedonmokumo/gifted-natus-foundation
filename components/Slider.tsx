0dee83d3f67e607e718062b8cc7d1ef050831df4
tonwareemi zakana <tonwareemizakana@gmail.com>
2024-11-25 18:37:20 +0100

    latest new changes

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -22,9 +22,7 @@ const Slider = () => {
         if (blogPostFetch.status === 200) {
           const posts: Post[] = await blogPostFetch.json();
           // Sort posts by date and pick the latest one
-          const sortedPosts = posts.sort(
-            (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-          );
+          const sortedPosts = posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
           setLatestPost(sortedPosts);
         }
       } catch (error) {
@@ -40,37 +38,27 @@ const Slider = () => {
       {/* Slider Container */}
       <div className="flex overflow-x-scroll space-x-4 lg:w-[80%] mx-auto no-scrollbar">
         {/* Display Latest Post */}
-        <div className="min-w-[300px] overflow-hidden flex space-x-5" style={{maxWidth: "calc(300px * 5)"}}>
-          {latestPost &&
-            latestPost.map((latestPost) => (
-              <div className="max-w-[300px] bg-gray-900 text-white p-5 rounded-lg flex-shrink-0">
-                <img
-                  className="rounded-lg w-full h-40 object-cover"
-                  src={latestPost.img}
-                  alt={latestPost.title}
-                />
-                <Link
-                  href={`/blog/${latestPost._id}`}
-                  className="mt-4 text-lg font-bold"
-                >
-                  {latestPost.title}
-                </Link>
-                <div className="flex justify-between items-center text-sm mt-4">
-                  <span>
-                    {latestPost.views} views{" "}
-                    <FaEye className="text-[#07a034] text-1xl inline-block" />
-                  </span>
-                </div>
-              </div>
-            ))}
-        </div>
+        {latestPost && latestPost.map( (latestPost)=>(
+          <div className="max-w-[300px] bg-gray-900 text-white p-5 rounded-lg flex-shrink-0">
+            <img
+              className="rounded-lg w-full h-40 object-cover"
+              src={latestPost.img}
+              alt={latestPost.title}
+            />
+            <Link href={`/blog/${latestPost._id}`} className="mt-4 text-lg font-bold">
+              {latestPost.title}
+            </Link>
+            <div className="flex justify-between items-center text-sm mt-4">
+              <span>
+                {latestPost.views} views <FaEye className="text-[#07a034] text-1xl inline-block" />
+              </span>
+            </div>
+          </div>
+        ))}
       </div>
       {/* See All */}
       <div className="mt-8 text-center">
-        <a
-          href="/blog"
-          className="text-lg text-green-500 font-semibold hover:underline"
-        >
+        <a href="/blog" className="text-lg text-green-500 font-semibold hover:underline">
           See all →
         </a>
       </div>
